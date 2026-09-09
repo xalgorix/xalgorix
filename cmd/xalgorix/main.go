f32dc0d426132f7d52fe76fb1afdf7bce10e68d8
xalgord <xalgord@gmail.com>
2026-09-10 02:09:10 +0530

    release: v4.6.75

--- cmd/xalgorix/main.go
+++ cmd/xalgorix/main.go
@@ -32,7 +32,7 @@ import (
 // The hardcoded fallback is only used when developers `go run` the package
 // without ldflags. It is a `var` (not `const`) precisely so ldflags can
 // rewrite it.
-var version = "4.6.74"
+var version = "4.6.75"
 
 const defaultWebPort = 9137
 
